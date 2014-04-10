ca66d1f159967f7ce4ebc32d649339a73c1802b8
xbomber360@gmail.com <xbomber360@gmail.com@815a6145-604a-79f8-63d4-5b41fe8524c0>
2014-04-10 13:05:14 +0000

    implementazione prodotto manager

--- Piattaforme/Piattaforme-ejb/src/java/ejb/manager/ProdottoManager.java
+++ Piattaforme/Piattaforme-ejb/src/java/ejb/manager/ProdottoManager.java
@@ -3,9 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ejb.manager;
 
+import entity.Categoria;
+import entity.Marca;
 import entity.Prodotto;
 import exception.ProdottoNonTrovatoException;
 import facade.ProdottoFacade;
@@ -18,28 +19,67 @@ import javax.persistence.EntityManager;
  */
 @Stateless
 public class ProdottoManager implements ProdottoManagerLocal {
-    
-    EntityManager em ;
-    ProdottoFacade pf ;
+
+    EntityManager em;
+    ProdottoFacade pf;
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
-
     @Override
-    public boolean controllaQuantita(Long idProdotto, int quantita) throws ProdottoNonTrovatoException{
-        
+    public boolean controllaQuantita(Long idProdotto, int quantita) throws ProdottoNonTrovatoException {
+
         Prodotto p = pf.find(idProdotto);
-        if(p == null)
+        if (p == null) {
             throw new ProdottoNonTrovatoException();
-        
+        }
+
         return p.getQuantita() >= quantita;
     }
 
+   
+
     @Override
+    public void aggiungiCategoria(Categoria c) {
+    }
+
+    @Override
+    public void aggiungiMarca(Marca m) {
+    }
+
+    @Override
+    public void aggiungiProdotto(Prodotto p, Marca m, Categoria c) {
+    }
+
+    @Override
+    public void modificaQuantitaProdotto(Prodotto p, int quantita) {
+    }
+
+    @Override
+    public void rimuoviCategoria(Categoria c) {
+    }
+
+    @Override
+    public void rimuoviProdotto(Prodotto p) {
+        
+        
+    }
+    
+     @Override
     public Prodotto cercaProdottoPerId(Long idProdotto) {
+        return null;
+    }
+
+    @Override
+    public List<Prodotto> cercaProdottoPerNome(String parameter) {
+        return null;
+    }
+
+    @Override
+    public List<Prodotto> cercaTuttiProdotti() {
+        return null;
     }
     
     
-    
+
     
 }
